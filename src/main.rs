1842020eb1a3e106bbd9e9c1a60f64902c435791
Hadar Greinsmark <6073104+HadarGreinsmark@users.noreply.github.com>
2023-08-18 17:26:31 +0200

    Add time zone localization and day differentiation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 mod score;
 
+use chrono::{self, TimeZone};
 use std::io::Read;
+
+fn time_of_day_str(timestamp: i64) -> String {
+    let time = chrono::Local.timestamp_opt(timestamp, 0).unwrap();
+    time.format("%Y-%m-%d %H:%M").to_string()
+}
+
+fn duration_str(secs: u32) -> String {
+    // gives string in hours and mins
+    let hours = secs / 3600;
+    let mins = (secs % 3600) / 60;
+    format!("{:2}h {:2}m", hours, mins)
+}
 
 fn main() {
     let mut score = score::Score::new();
@@ -8,7 +21,16 @@ fn main() {
     while let Ok(_) = std::io::stdin().read_exact(&mut buffer) {
         let timestamp = u32::from_be_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]);
         let keypresses = buffer[4];
-        score.insert(timestamp, keypresses);
-        println!("{:9.0}, {:3}", score.calculate(), keypresses);
+        score.append(timestamp, keypresses);
+
+        println!(
+            "{} {:9.0}; {:3}; total work {}; {}; LoR {}",
+            time_of_day_str(timestamp as i64 * 5),
+            score.current_score(),
+            keypresses,
+            duration_str(score.total_work()),
+            duration_str(score.needed_recovery()),
+            duration_str(score.lack_of_recovery())
+        );
     }
 }
--- src/score.rs
+++ src/score.rs
@@ -1,13 +1,15 @@
-const INTERVAL_LEN_SECS: u32 = 5;
+use chrono::{self, TimeZone};
 
-const LACK_OF_RECOVERY_INTERVAL_PENALTY: f32 = 0.1 / (60.0 * 60.0 / INTERVAL_LEN_SECS as f32); // 0.1 per hour
+const SLICE_SIZE: u32 = 5;
+
+const LACK_OF_RECOVERY_INTERVAL_PENALTY: f32 = 0.1 / (60.0 * 60.0 / SLICE_SIZE as f32); // 0.1 per hour
 
 fn duration_multiplier(intervals: u32) -> f32 {
-    if intervals <= 180 * 60 / INTERVAL_LEN_SECS {
+    if intervals <= 180 * 60 / SLICE_SIZE {
         2.0
-    } else if intervals <= 240 * 60 / INTERVAL_LEN_SECS {
+    } else if intervals <= 240 * 60 / SLICE_SIZE {
         1.5
-    } else if intervals <= 480 * 60 / INTERVAL_LEN_SECS {
+    } else if intervals <= 480 * 60 / SLICE_SIZE {
         1.0
     } else {
         // intervals <= 480 * 60 / INTERVAL_LEN_SECS
@@ -15,54 +17,87 @@ fn duration_multiplier(intervals: u32) -> f32 {
     }
 }
 
+/// Returns true if the two time slices are on the same day in the local timezone
+fn is_same_day(time_slice1: u32, time_slice2: u32) -> bool {
+    let time1 = chrono::Local
+        .timestamp_opt(time_slice1 as i64 * SLICE_SIZE as i64, 0)
+        .unwrap();
+    let time2 = chrono::Local
+        .timestamp_opt(time_slice2 as i64 * SLICE_SIZE as i64, 0)
+        .unwrap();
+    time1.date_naive() == time2.date_naive()
+}
+
 pub struct Score {
-    // A tuple containing timestamp / 5, and the number of key presses
-    series: Vec<(u32, u8)>,
+    previous_time_slice: u32,
+    sum_keypresses: u32,
+    total_work_intervals: u32,
+    recovery_debt: u32,
+    num_lack_of_recovery_cycles: u32,
 }
 
 impl Score {
     pub fn new() -> Score {
-        Score { series: Vec::new() }
+        Score {
+            previous_time_slice: 0,
+            sum_keypresses: 0,
+            total_work_intervals: 0,
+            recovery_debt: 0,
+            num_lack_of_recovery_cycles: 0,
+        }
     }
 
-    pub fn insert(&mut self, timestamp: u32, keypresses: u8) {
-        self.series.push((timestamp, keypresses));
+    fn reset(&mut self) {
+        self.previous_time_slice = 0;
+        self.sum_keypresses = 0;
+        self.total_work_intervals = 0;
+        self.recovery_debt = 0;
+        self.num_lack_of_recovery_cycles = 0;
     }
 
-    pub fn calculate(&self) -> f32 {
-        let mut previous_timestamp = 0;
-        let mut sum_keypresses = 0;
-        let mut total_work_intervals: u32 = 0;
-        let mut recovery_debt: u32 = 0;
-        let mut num_lack_of_recovery_intervals = 0;
+    pub fn append(&mut self, time_slice: u32, keypresses: u8) {
+        if !is_same_day(self.previous_time_slice, time_slice) {
+            self.reset();
+        }
+        self.sum_keypresses += keypresses as u32;
 
-        // Iterate over the series
-        for (timestamp, keypresses) in self.series.iter() {
-            // Add the keypresses to the sum
-            sum_keypresses += *keypresses as u32;
-
-            let missing_intervals = *timestamp - previous_timestamp - 1;
-            if missing_intervals >= 3 {
-                recovery_debt -= 6 * missing_intervals;
-            }
-
-            assert!(*keypresses != 0);
-            total_work_intervals += 1;
-            recovery_debt += 1;
-
-            if recovery_debt >= 60 * 60 / INTERVAL_LEN_SECS {
-                // 1 hour
-                num_lack_of_recovery_intervals += 1;
-            }
-            previous_timestamp = *timestamp;
+        let missing_intervals = time_slice - self.previous_time_slice - 1;
+        if missing_intervals >= 3 {
+            // Do "self.recovery_debt -= 6 * missing_intervals;" but make sure self.recovery_debt doesn't go below 0
+            self.recovery_debt = self.recovery_debt.saturating_sub(6 * missing_intervals);
         }
 
+        assert!(keypresses != 0);
+        self.total_work_intervals += 1;
+        self.recovery_debt += 1;
+
+        if self.recovery_debt >= 60 * 60 / SLICE_SIZE {
+            // 1 hour
+            self.num_lack_of_recovery_cycles += 1;
+        }
+        self.previous_time_slice = time_slice;
+    }
+
+    pub fn current_score(&self) -> f32 {
         let lack_of_recovery_factor =
-            1.0 - num_lack_of_recovery_intervals as f32 * LACK_OF_RECOVERY_INTERVAL_PENALTY;
-        // sum_keypresses / (30 * FORCE_FACTOR * POSTURE_FACTOR * ADDITIONAL_FACTORS * REPETITIVENESS_MULTIPLIER * (duration_interval_mins * lack_of_recovery_factor * duration_multiplier(duration_interval_mins)))
-        sum_keypresses as f32
-            / (total_work_intervals as f32
+            1.0 - self.num_lack_of_recovery_cycles as f32 * LACK_OF_RECOVERY_INTERVAL_PENALTY;
+        // self.sum_keypresses / (30 * FORCE_FACTOR * POSTURE_FACTOR * ADDITIONAL_FACTORS * REPETITIVENESS_MULTIPLIER * (duration_interval_mins * lack_of_recovery_factor * duration_multiplier(duration_interval_mins)))
+        self.sum_keypresses as f32
+            / (self.total_work_intervals as f32
                 * lack_of_recovery_factor
-                * duration_multiplier(total_work_intervals))
+                * duration_multiplier(self.total_work_intervals))
+            * 10.0
+    }
+
+    pub fn total_work(&self) -> u32 {
+        self.total_work_intervals * SLICE_SIZE
+    }
+
+    pub fn needed_recovery(&self) -> u32 {
+        self.recovery_debt / 6 * SLICE_SIZE
+    }
+
+    pub fn lack_of_recovery(&self) -> u32 {
+        self.num_lack_of_recovery_cycles * SLICE_SIZE
     }
 }
