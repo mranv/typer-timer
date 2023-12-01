3cd0ea969fc44b6ca39acff6a33ea3754f6e9dac
Hadar Greinsmark <6073104+HadarGreinsmark@users.noreply.github.com>
2023-12-01 18:01:47 +0100

    Calculare work ranges and track micro pauses

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
         score.append(timestamp, keypresses);
 
         println!(
-            "{} {:9.0}; {:5}; {:3}; total work {}; rest {}; LoR {}; last recovery {}",
+            "{} {:9.0}; {:5}; {:3}; total work {}; rest {}; LoR {}; μ/d {:2.0}%; μ/h {:2.0}%; last recovery {}",
             time_of_day_str(timestamp as i64 * 5),
             score.current_score(),
             score.total_keypresses(),
@@ -32,7 +32,31 @@ fn main() {
             duration_str(score.total_work()),
             duration_str(score.needed_recovery()),
             duration_str(score.lack_of_recovery()),
+            score.micro_pause_share() * 100.0,
+            score.micro_pause_share_past_hour() * 100.0,
             duration_str(score.last_recovery_since()),
         );
+
+        // print work ranges in chronological order, if its work or rest, and the duration
+        let work_ranges = score.work_ranges();
+        let mut current_is_work = false;
+        let mut current_begin = score.day_start();
+        let mut current_end = current_begin;
+        for time_slice in work_ranges {
+            if time_slice > 60 * 10 {
+                println!(
+                    "    {} -- {} {} {}",
+                    time_of_day_str(current_begin as i64),
+                    time_of_day_str(current_end as i64),
+                    if current_is_work { "work" } else { "rest" },
+                    duration_str(current_end - current_begin),
+                );
+                current_is_work = !current_is_work;
+                current_begin = current_end;
+                current_end = current_begin + time_slice;
+            } else {
+                current_end += time_slice;
+            }
+        }
     }
 }
--- src/score.rs
+++ src/score.rs
@@ -1,4 +1,5 @@
 use chrono::{self, TimeZone};
+use std::collections::VecDeque;
 
 const SLICE_SIZE: u32 = 5;
 const REST_TO_WORK_RATIO: u32 = 4;
@@ -30,54 +31,89 @@ fn is_same_day(time_slice1: u32, time_slice2: u32) -> bool {
 }
 
 pub struct Score {
+    day_start_time_slice: u32,
     previous_time_slice: u32,
     sum_keypresses: u32,
-    total_work_slices: u32,
+    micro_pause_slices: u32,
+    work_slices: u32,
     recovery_debt: u32,
     num_lack_of_recovery_slices: u32,
     last_recovery_slice: u32,
+    // Tuple of (number of slices, True: work, False: rest)
+    work_ranges: Vec<u32>,
+    // Queue of (time_slice, micro_pause_slices, work_slices) tuple. We consume the queue from the front, so that only the last 60 minutes are kept.
+    last_hour_slices: VecDeque<(u32, u32, u32)>,
 }
 
 impl Score {
     pub fn new() -> Score {
         Score {
+            day_start_time_slice: 0,
             previous_time_slice: 0,
             sum_keypresses: 0,
-            total_work_slices: 0,
+            micro_pause_slices: 0,
+            work_slices: 0,
             recovery_debt: 0,
             num_lack_of_recovery_slices: 0,
             last_recovery_slice: 0,
+            work_ranges: vec![(0)],
+            last_hour_slices: VecDeque::new(),
         }
     }
 
     fn reset(&mut self) {
+        self.day_start_time_slice = 0;
         self.previous_time_slice = 0;
         self.sum_keypresses = 0;
-        self.total_work_slices = 0;
+        self.micro_pause_slices = 0;
+        self.work_slices = 0;
         self.recovery_debt = 0;
         self.num_lack_of_recovery_slices = 0;
         self.last_recovery_slice = 0;
+        self.work_ranges = vec![(0)];
+        self.last_hour_slices = VecDeque::new();
     }
 
     pub fn append(&mut self, time_slice: u32, keypresses: u8) {
+        let mut hour_slice = (time_slice, 0, 0);
+        if keypresses == 0 {
+            println!(
+                "Warning: keypresses == 0 at {}",
+                chrono::Local
+                    .timestamp_opt(time_slice as i64 * SLICE_SIZE as i64, 0)
+                    .unwrap()
+                    .format("%Y-%m-%d %H:%M")
+            );
+            return;
+        }
         if !is_same_day(self.previous_time_slice, time_slice) {
+            println!("New day");
             self.reset();
+            self.last_recovery_slice = time_slice;
+            self.previous_time_slice = time_slice;
+            self.day_start_time_slice = time_slice;
         }
         self.sum_keypresses += keypresses as u32;
 
-        let missing_slices = time_slice - self.previous_time_slice - 1;
-        if missing_slices >= 60 * 5 / SLICE_SIZE {
+        let missing_slices = (time_slice - self.previous_time_slice).saturating_sub(1);
+        if missing_slices < 3 {
+            // Count as work because it's too short to be a micro pause
+            self.work_slices += missing_slices;
+        } else if missing_slices < 60 * 5 / SLICE_SIZE {
+            self.micro_pause_slices += missing_slices;
+            hour_slice.1 += missing_slices;
+        } else {
             // Do "self.recovery_debt -= REST_TO_WORK_RATIO * missing_intervals;" but make sure self.recovery_debt doesn't go below 0
             self.recovery_debt = self
                 .recovery_debt
                 .saturating_sub(REST_TO_WORK_RATIO * missing_slices);
             self.last_recovery_slice = time_slice;
-        } else {
-            self.total_work_slices += missing_slices;
+            self.work_ranges.push(missing_slices);
+            self.work_ranges.push(0);
         }
 
-        assert!(keypresses != 0);
-        self.total_work_slices += 1;
+        self.work_slices += 1;
+        hour_slice.2 += 1;
         self.recovery_debt += 1;
 
         if self.recovery_debt >= 60 * 60 / SLICE_SIZE {
@@ -85,16 +121,27 @@ impl Score {
             self.num_lack_of_recovery_slices += 1;
         }
         self.previous_time_slice = time_slice;
+        // Increment last work range
+        *self.work_ranges.last_mut().unwrap() += 1;
+        // Remove elements from the front of the queue until the first element is less than 60 minutes old
+        while !self.last_hour_slices.is_empty()
+            && self.last_hour_slices[0].0 + 60 * 60 / SLICE_SIZE < time_slice
+        {
+            self.last_hour_slices.pop_front().unwrap();
+        }
+
+        self.last_hour_slices.push_back(hour_slice);
     }
 
     pub fn current_score(&self) -> f32 {
+        let total_work_slices = self.work_slices + self.micro_pause_slices;
         let lack_of_recovery_factor =
             1.0 - self.num_lack_of_recovery_slices as f32 * LACK_OF_RECOVERY_SLICE_PENALTY;
         // self.sum_keypresses / (30 * FORCE_FACTOR * POSTURE_FACTOR * ADDITIONAL_FACTORS * REPETITIVENESS_MULTIPLIER * (duration_interval_mins * lack_of_recovery_factor * duration_multiplier(duration_interval_mins)))
         self.sum_keypresses as f32
-            / (self.total_work_slices as f32
+            / (total_work_slices as f32
                 * lack_of_recovery_factor
-                * duration_multiplier(self.total_work_slices))
+                * duration_multiplier(total_work_slices))
             * 10.0
     }
 
@@ -102,8 +149,24 @@ impl Score {
         self.sum_keypresses
     }
 
+    pub fn micro_pause_share(&self) -> f32 {
+        1.0 - self.micro_pause_slices as f32 * SLICE_SIZE as f32 / self.total_work() as f32
+    }
+
+    pub fn micro_pause_share_past_hour(&self) -> f32 {
+        let mut total_micro_pause_slices = 0;
+        let mut total_work_slices = 0;
+        for (_, micro_pause_slices, work_slices) in &self.last_hour_slices {
+            total_micro_pause_slices += micro_pause_slices;
+            total_work_slices += work_slices;
+        }
+        1.0 - total_micro_pause_slices as f32
+            / (total_micro_pause_slices + total_work_slices) as f32
+    }
+
     pub fn total_work(&self) -> u32 {
-        self.total_work_slices * SLICE_SIZE
+        let total_work_slices = self.work_slices + self.micro_pause_slices;
+        total_work_slices * SLICE_SIZE
     }
 
     pub fn last_recovery_since(&self) -> u32 {
@@ -116,5 +179,17 @@ impl Score {
 
     pub fn lack_of_recovery(&self) -> u32 {
         self.num_lack_of_recovery_slices * SLICE_SIZE
+    }
+
+    pub fn day_start(&self) -> u32 {
+        self.day_start_time_slice * SLICE_SIZE
+    }
+
+    pub fn work_ranges(&self) -> Vec<u32> {
+        let mut work_ranges = Vec::new();
+        for duration in &self.work_ranges {
+            work_ranges.push(*duration * SLICE_SIZE);
+        }
+        work_ranges
     }
 }
