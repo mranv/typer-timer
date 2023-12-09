cbdd533aa6e0693f2ea4eede2fb2776c27aae80a
Hadar Greinsmark <6073104+HadarGreinsmark@users.noreply.github.com>
2023-12-09 11:47:51 +0100

    Add basic Gnome extension showing banner

---
+++ gnome-extension/extension.js
@@ -0,0 +1,42 @@
+const { St, Clutter } = imports.gi;
+const Main = imports.ui.main;
+
+let panelButton;
+
+const GLib = imports.gi.GLib;
+
+function init() {
+    log("Initializing the extension");
+    // Create a Button with initial text
+    panelButton = new St.Bin({
+        style_class: "panel-button",
+    });
+    let panelButtonText = new St.Label({
+        text: "Loading...",
+        y_align: Clutter.ActorAlign.CENTER,
+    });
+    panelButton.set_child(panelButtonText);
+
+
+    let decoder = new TextDecoder();
+        
+    // Read and update the text every second
+    GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+        let [success, contents] = GLib.file_get_contents("/tmp/repeto/banner");
+        contents_str = decoder.decode(contents)
+        if (success) {
+            panelButtonText.set_text(contents_str);
+        }    
+        return true;
+    });
+}
+
+function enable() {
+    // Add the button to the panel
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+}
+
+function disable() {
+    // Remove the added button from panel
+    Main.panel._rightBox.remove_child(panelButton);
+}
